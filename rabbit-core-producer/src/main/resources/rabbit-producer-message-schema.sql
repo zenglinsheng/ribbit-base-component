991710bcc8808b5ba05a312c902962cbdaf2f43e
zenglsh <996433583@qq.com>
2024-04-30 15:00:45 +0800

    add:rabbitmq学习笔记

--- rabbit-core-producer/src/main/resources/rabbit-producer-message-schema.sql
+++ rabbit-core-producer/src/main/resources/rabbit-producer-message-schema.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS `broker_message` (
   `message` varchar(4000),
   `try_count` int(4) DEFAULT 0,
   `status` varchar(10) DEFAULT '',
-  `next_retry` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `create_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `next_retry` timestamp NOT NULL,
+  `create_time` timestamp NOT NULL,
+  `update_time` timestamp NOT NULL,
   PRIMARY KEY (`message_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
